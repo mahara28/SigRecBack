36f49f28554d67a0bc9172df7dcc52875b14350b
mahara28 <maha.riahi28@gmail.com>
2026-03-26 09:32:30 +0100

    recup

--- src/main/java/com/mc/back/sigrecette/model/AdmFonc.java
+++ src/main/java/com/mc/back/sigrecette/model/AdmFonc.java
@@ -51,9 +51,7 @@ public class AdmFonc {
     @Column(name = "des_en", length = 300)
     private String desEn;
     
-    @Size(max = 300)
-    @Column(name = "code_translate", length = 300)
-    private String codeTranslate;
+
 
     @Transient
     private List<AdmFonc> listSousMenu;
